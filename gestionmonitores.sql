6c6eb7c2457c4726b81ef448768dd6dcd557fcf5
Alvaro-PC\Alvaro <alvaro.castanol@autonoma.edu.co>
2017-03-26 22:35:52 -0500

    Base de datos cambio en nombre de campo password por pwd y email UNIQUE

--- gestionmonitores.sql
+++ gestionmonitores.sql
@@ -3,14 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-03-2017 a las 21:14:26
+-- Tiempo de generación: 27-03-2017 a las 05:34:19
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
-
-DROP DATABASE IF EXISTS gestionmonitores;
-CREATE DATABASE IF NOT EXISTS gestionmonitores;
-GRANT ALL PRIVILEGES ON gestionmonitores.* TO 'admin'@'localhost';
-FLUSH PRIVILEGES;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,21 +27,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `tbl_docentes` (
-  `cedula` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `apellidos` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `telefono` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `direccion` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `cedula` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `apellidos` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `direccion` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `fechaNacimiento` date DEFAULT NULL,
-  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `pwd` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `tbl_docentes`
 --
 
-INSERT INTO `tbl_docentes` (`cedula`, `nombre`, `apellidos`, `telefono`, `direccion`, `fechaNacimiento`, `email`, `password`) VALUES
+INSERT INTO `tbl_docentes` (`cedula`, `nombre`, `apellidos`, `telefono`, `direccion`, `fechaNacimiento`, `email`, `pwd`) VALUES
 ('0987654321', 'Jorge Iván', 'Meza Martinez', NULL, NULL, NULL, 'jorge.m@autonoma.edu.co', '123'),
 ('1234567890', 'Sandra', 'Victoria Hurtado', NULL, NULL, NULL, 'sandra.v@autonoma.edu.co', '123');
 
@@ -57,21 +52,21 @@ INSERT INTO `tbl_docentes` (`cedula`, `nombre`, `apellidos`, `telefono`, `direcc
 --
 
 CREATE TABLE `tbl_estudiantes` (
-  `cedula` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `apellido` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `cedula` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `apellido` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `fechaNacimiento` date NOT NULL,
-  `password` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `telefono` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `direccion` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL
+  `pwd` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `direccion` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `tbl_estudiantes`
 --
 
-INSERT INTO `tbl_estudiantes` (`cedula`, `nombre`, `apellido`, `email`, `fechaNacimiento`, `password`, `telefono`, `direccion`) VALUES
+INSERT INTO `tbl_estudiantes` (`cedula`, `nombre`, `apellido`, `email`, `fechaNacimiento`, `pwd`, `telefono`, `direccion`) VALUES
 ('12345', 'Alvaro', 'Castaño', 'alvaro@hot.com', '2013-05-05', '123', '8882040', 'la Cumbre'),
 ('1234567890', 'Yasmin', 'Montoya', 'yasmin@hot.com', '2017-04-23', '123', '8883066', 'Malabar'),
 ('67890', 'Ruby', 'Lopez', 'ruby@hot.com', '1995-01-16', '123', '8883066', 'Camilo torres');
