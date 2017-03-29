bd5f45d985b09ada74b19b0e458957460f89edad
Alvaro-PC\Alvaro <alvaro.castanol@autonoma.edu.co>
2017-03-28 19:26:55 -0500

    Actualizacion List Estudiantes en EstudianteDAO

--- src/java/com/dao/gestionmonitores/EstudianteDAO.java
+++ src/java/com/dao/gestionmonitores/EstudianteDAO.java
@@ -1,9 +1,11 @@
 package com.dao.gestionmonitores;
 
 import com.entidades.gestionmonitores.TblDocentes;
+import com.entidades.gestionmonitores.TblEstudiantes;
 import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
@@ -13,12 +15,11 @@ public class EstudianteDAO {
 
     public boolean buscar(String email, String pwd) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        //session.beginTransaction();
         String sql = "from TblEstudiantes e where e.email=:email and e.pwd=:pwd";
         Query query = session.createQuery(sql);
         query.setParameter("email", email);
         query.setParameter("pwd", pwd);
-        List<TblDocentes> list = query.list();
+        List<TblEstudiantes> list = query.list();
         if (list.size() > 0) {
             session.close();
             return true;
